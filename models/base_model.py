2203153e1807b8d65e96596e403653417d1d28da
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-06-28 12:55:38 -0700

    BaseModel Class

---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module: BaseModel
+"""
+import models
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel():
+    """Class: BaseModel:Defines bases 
+       for the rest of the classes
+    """
+    def __init__(self, *args, **kwargs):
+    """Initializing BaseModel"""
+     if 
+
