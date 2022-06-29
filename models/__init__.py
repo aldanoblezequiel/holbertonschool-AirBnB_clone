ff11f748d7d07df9089a6d10548d748a6b659b1e
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-06-29 13:39:50 -0700

    ..

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Module: FileStorage"""
+
+
 from models.engine.file_storage import FileStorage
 
+""" creates variable storage to call reload """
 
 storage = FileStorage()
 storage.reload()
