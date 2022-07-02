058c4ccc7a6455d79411f9d21a18d4ca8d5562f1
NazarenaAranda <nazarenaaranda15@gmail.com>
2022-07-02 16:42:44 -0700

    primer orueba

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,7 @@
 from models.engine.file_storage import FileStorage
 
 """Creating modile to reload"""
+
+
 storage = FileStorage()
 storage.reload()
--- models/city.py
+++ models/city.py
@@ -9,6 +9,3 @@ class City(BaseModel):
     """Class: City"""
     state_id = ""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
